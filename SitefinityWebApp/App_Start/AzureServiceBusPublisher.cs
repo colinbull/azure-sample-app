c497684bda86d360929f5457814522981f8d6754
Colin Bull <colinbul@gmail.com>
2019-02-06 22:44:58 +0000

    Content manager event

--- SitefinityWebApp/App_Start/AzureServiceBusPublisher.cs
+++ SitefinityWebApp/App_Start/AzureServiceBusPublisher.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using ServiceStack.Text;
 using Telerik.Sitefinity.Abstractions;
 using Telerik.Sitefinity.Data;
+using Telerik.Sitefinity.Modules.GenericContent;
 using Telerik.Sitefinity.Modules.Pages;
 using Telerik.Sitefinity.Pages.Model;
 using Telerik.Sitefinity.Publishing;
@@ -23,12 +24,19 @@ namespace SitefinityWebApp {
             if (e.CommandName == "Bootstrapped")
             {
                 PublishingManager.Executing += OnPublished;
+                ContentManager.Executing += OnContentEvent;
             }
+        }
+
+        private static void OnContentEvent(object sender, ExecutingEventArgs e)
+        {
+            Trace.WriteLine($"AzureServicePublisher: Executing content event {e.GetType().FullName} {e.Dump()}");
         }
 
         private static void OnPublished(object sender, ExecutingEventArgs e)
         {
-            Trace.WriteLine($"AzureServicePublisher: Executing event {e.Dump()}");
+            Trace.WriteLine($"AzureServicePublisher: Executing publishing event {e.GetType().FullName} {e.Dump()}");
+   
         }
 
     }
