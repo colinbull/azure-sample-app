2288646b9fc5ba86ed6e0186f02d1f14defae782
Colin Bull <colinbul@gmail.com>
2019-02-06 22:08:55 +0000

    Basic tracing does it even work

--- SitefinityWebApp/App_Start/AzureServiceBusPublisher.cs
+++ SitefinityWebApp/App_Start/AzureServiceBusPublisher.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using ServiceStack.Text;
 using Telerik.Sitefinity.Abstractions;
 using Telerik.Sitefinity.Data;
 using Telerik.Sitefinity.Modules.Pages;
@@ -11,59 +12,21 @@ using Telerik.Sitefinity.Security;
 namespace SitefinityWebApp {
     public static class AzureServiceBusPublisher {
         public static void Start() {
+         Trace.WriteLine("AzureServicePublisher Starting");
          Bootstrapper.Initialized += new EventHandler<ExecutedEventArgs>(Bootstrapper_Initialized);
         }
-        static void Bootstrapper_Initialized(object sender, Telerik.Sitefinity.Data.ExecutedEventArgs e)
+        public static void Bootstrapper_Initialized(object sender, Telerik.Sitefinity.Data.ExecutedEventArgs e)
         {
+            
+            Trace.WriteLine("AzureServicePublisher: Bootstrapped");
             if (e.CommandName == "Bootstrapped")
             {
                 PageManager.Executing += new EventHandler<Telerik.Sitefinity.Data.ExecutingEventArgs>(PageManager_Executing);
             }
         }
-        private static void PageManager_Executing(object sender, Telerik.Sitefinity.Data.ExecutingEventArgs e)
+        public static void PageManager_Executing(object sender, Telerik.Sitefinity.Data.ExecutingEventArgs e)
         {
-            if (e.CommandName == "CommitTransaction" || e.CommandName == "FlushTransaction")
-            {
-                var provider = sender as PageDataProvider;
-                var dirtyItems = provider.GetDirtyItems();
-                if (dirtyItems.Count != 0)
-                {
-                    foreach (var item in dirtyItems)
-                    {
-                        //Can be New, Updated, or Deleted
-                        SecurityConstants.TransactionActionType itemStatus = provider.GetDirtyItemStatus(item);
-                        var pageData = item as PageData;
-                        var url = System.Web.HttpContext.Current.Request.Url.ToString();
-                        if (pageData != null)
-                        {
-                            if (itemStatus == SecurityConstants.TransactionActionType.Updated)
-                            {
-                                Trace.WriteLine($"Publish Event {url}");
-                                //Publish
-//                                if (url.Contains("workflowOperation=Publish"))
-//                                {
-//                                    
-//                                }
-//                                //Batch Publish
-//                                if (url.Contains("/batchPublishDraft/"))
-//                                {
-//                                    //Your logic here
-//                                }
-//                                //Batch unpublish
-//                                if (url.Contains("/batchUnpublishPage/"))
-//                                {
-//                                    //Your logic here
-//                                }
-//                                //Save as Draft
-//                                if (url.Contains("workflowOperation=SaveDraft"))
-//                                {
-//                                    //Your logic here
-//                                }
-                            }
-                        }
-                    }
-                }
-            }
+            Trace.WriteLine($"AzureServicePublisher: Executing event {e.Dump()}");
         }
     }
     
